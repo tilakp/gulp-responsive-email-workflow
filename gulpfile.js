d82dfbb9b2b2eadb1ca35c51d67b28fa4b72c601
Tilak Patel <tilak[dot]patel[at]gmail[dot]com>
2022-11-21 23:48:31 -0800

    upgrade to gulp 4 and added html templates

--- gulpfile.js
+++ gulpfile.js
@@ -5,23 +5,26 @@ var inlineCss = require('gulp-inline-css');
 var fileinclude = require('gulp-file-include');
 var template = require('gulp-template');
 var glob = require("glob");
-var sass = require('gulp-sass');
+const sass = require('gulp-sass')(require('sass'));
 var extname = require('gulp-extname');
 var assemble = require('assemble');
 var app = assemble();
 var inlineSource = require('gulp-inline-source');
 
-// Static server
-gulp.task('browser-sync', ['build-preview'], function() {
-    browserSync.init({
-        server: {
-            baseDir: "./"
-        },
-        startPath: "/preview/"
-    });
+// Build Templates
+gulp.task('build-templates', async function() {
+    return gulp.src('./src/templates/*.html')
+        .pipe(fileinclude({
+            prefix: '@@',
+            basepath: '@file'
+        }))
+        .pipe(inlineCss({
+            preserveMediaQueries: true
+        }))
+        .pipe(gulp.dest('./build/'));
 });
 
-gulp.task('load', function(cb) {
+gulp.task('load', async function(cb) {
   app.partials('src/hb-templates/partials/**/*.hbs');
   app.layouts('src/hb-templates/layouts/*.hbs');
   app.pages('src/hb-templates/emails/*.hbs');
@@ -31,18 +34,18 @@ gulp.task('load', function(cb) {
   cb();
 });
 
-gulp.task('styles', function() {
+gulp.task('styles', async function() {
     gulp.src('src/hb-templates/css/**/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('build/css/'));
 });
 
-gulp.task('images', function() {
+gulp.task('images', async function() {
     gulp.src('src/hb-templates/img/**/*.png')
         .pipe(gulp.dest('build/img/'));
 });
 
-gulp.task('assemble', ['load', 'styles', 'images'], function() {
+gulp.task('assemble', gulp.series('load', 'styles', 'images', async function() {
   return app.toStream('pages')
     .pipe(app.renderFile())
     .pipe(extname())
@@ -53,21 +56,21 @@ gulp.task('assemble', ['load', 'styles', 'images'], function() {
         preserveMediaQueries: true
     }))
     .pipe(app.dest('build'));
-});
+}));
 
-gulp.task('preview-sass', function(){
+gulp.task('preview-sass', async function(){
   return gulp.src('./src/preview/scss/*.scss')
     .pipe(sass().on('error', sass.logError)) // Converts Sass to CSS with gulp-sass
     .pipe(gulp.dest('./preview/css'))
 });
 
-gulp.task('preview-img', function(){
+gulp.task('preview-img', async function(){
     return gulp.src('./src/preview/img/**/*.png')
             .pipe(gulp.dest('./preview/img'));
 });
 
 // Build Preview
-gulp.task('build-preview', ['preview-sass', 'preview-img'], function() {
+gulp.task('build-preview', gulp.series('preview-sass', 'preview-img',  async function() {
     glob("./build/*.html", {}, function(er, files) {
         var templates = files.map(function(file) {
             var pathArray = file.split("/");
@@ -85,39 +88,40 @@ gulp.task('build-preview', ['preview-sass', 'preview-img'], function() {
             }))
             .pipe(gulp.dest('./preview'));
     });
-});
+}));
 
-// Build Templates
-gulp.task('build-templates', function() {
-    return gulp.src('./src/templates/*.html')
-        .pipe(fileinclude({
-            prefix: '@@',
-            basepath: '@file'
-        }))
-        .pipe(inlineCss({
-            preserveMediaQueries: true
-        }))
-        .pipe(gulp.dest('./build/'));
-});
+// Static server
+gulp.task('browser-sync', gulp.series('build-preview', async function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        },
+        startPath: "/preview/"
+    });
+}));
 
 // Watch Files For Changes And Reload
-gulp.task('watch', function() {
-    gulp.watch('./src/preview/**/*.html', ['build-preview', reload]);
-    gulp.watch('./src/preview/**/*.css', ['build-preview', reload]);
-    gulp.watch('./src/preview/**/*.scss', ['build-preview', reload]);
-    gulp.watch('./src/preview/**/*.png', ['build-preview', reload]);
+gulp.task('watch', async function() {
+    gulp.watch('./src/preview/**/*.html', gulp.series('build-preview', reload));
+    gulp.watch('./src/preview/**/*.css', gulp.series('build-preview', reload));
+    gulp.watch('./src/preview/**/*.scss', gulp.series('build-preview', reload));
+    gulp.watch('./src/preview/**/*.png', gulp.series('build-preview', reload));
 
-    gulp.watch('./src/templates/**/*.html', ['build-templates', reload]);
-    gulp.watch('./src/templates/css/*.css', ['build-templates', reload]);
+    gulp.watch('./src/templates/**/*.html', gulp.series('build-templates', reload));
+    gulp.watch('./src/templates/css/*.css', gulp.series('build-templates', reload));
     
-    gulp.watch('./src/hb-templates/**/*.css', ['assemble', reload]);
-    gulp.watch('./src/hb-templates/**/*.scss', ['assemble', reload]);
-    gulp.watch('./src/hb-templates/**/*.hbs', ['assemble', reload]);
-    gulp.watch('./src/hb-templates/**/*.png', ['assemble', reload]);
+    gulp.watch('./src/hb-templates/**/*.css', gulp.series('assemble', reload));
+    gulp.watch('./src/hb-templates/**/*.scss', gulp.series('assemble', reload));
+    gulp.watch('./src/hb-templates/**/*.hbs', gulp.series('assemble', reload));
+    gulp.watch('./src/hb-templates/**/*.png', gulp.series('assemble', reload));
 
     gulp.watch('./**/*.scss', reload);
     gulp.watch('./**/*.html', reload);
     gulp.watch('./**/*.css', reload);
 });
 
-gulp.task('default', ['build-templates', 'assemble', 'build-preview', 'browser-sync', 'watch']);
+// handle bar doesn't work still so removed it for now
+gulp.task('default',
+    gulp.series('build-templates',  'build-preview', 'browser-sync', 'watch'));
+  // gulp.series('build-templates', 'assemble', 'build-preview', 'browser-sync', 'watch'));
+
